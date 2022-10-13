05e0118fb50840dc3e6ead3bf3fa01d104bd0af0
nezd5553 <87045625+nezd5553@users.noreply.github.com>
2022-10-13 00:11:14 -0700

    Migrate config and cache files to new locations
    I doubled some stuff oops

--- run-Cemu.sh
+++ run-Cemu.sh
@@ -5,6 +5,10 @@ if [ ! -d "$HOME/.config/Cemu" ]; then
   mkdir "$HOME/.config/Cemu"
 fi
 
+if [ ! -d "$HOME/.cache/Cemu" ]; then
+  mkdir "$HOME/.cache/Cemu"
+fi
+
 if [ -f "$HOME/.local/share/Cemu/settings.xml" ]; then
   mv -b "$HOME/.local/share/Cemu/settings.xml" "$HOME/.config/Cemu"
 fi
@@ -29,31 +33,7 @@ if [ -d "$HOME/.local/share/Cemu/shaderCache" ]; then
   mv -b "$HOME/.local/share/Cemu/shaderCache" "$HOME/.cache/Cemu"
 fi
 
-# migrate old files to new location
-if [ ! -d "$HOME/.config/Cemu" ]; then
-  mkdir "$HOME/.config/Cemu"
-fi
-
-if [ -f "$HOME/.local/share/Cemu/settings.xml" ]; then
-  mv -b "$HOME/.local/share/Cemu/settings.xml" "$HOME/.config/Cemu"
-fi
-
-if [ -d "$HOME/.local/share/Cemu/controllerProfiles" ]; then
-  mv -b "$HOME/.local/share/Cemu/controllerProfiles" "$HOME/.config/Cemu"
-fi
-
-if [ -d "$HOME/.local/share/Cemu/gameProfiles" ]; then
-  mv -b "$HOME/.local/share/Cemu/gameProfiles" "$HOME/.config/Cemu"
-fi
-
-if [ -d "$HOME/.local/share/Cemu/debugger" ]; then
-  mv -b "$HOME/.local/share/Cemu/debugger" "$HOME/.config/Cemu"
-fi
-
-if [ -f "$HOME/.local/share/Cemu/network_services.xml" ]; then
-  mv -b "$HOME/.local/share/Cemu/network_services.xml" "$HOME/.config/Cemu"
-fi
-
+# delete old cemu executables
 if [ -f "$HOME/.local/share/Cemu/Cemu" ]; then
   rm "$HOME/.local/share/Cemu/Cemu"
 fi
