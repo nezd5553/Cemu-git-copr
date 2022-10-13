90b6a918a6b0ce8a04afabd0ee06347f5949c2ad
nezd5553 <87045625+nezd5553@users.noreply.github.com>
2022-10-13 00:05:33 -0700

    Migrate config and cache files to new locations
    Config now goes in .config/Cemu and, shaders go in .cache/Cemu.

--- run-Cemu.sh
+++ run-Cemu.sh
@@ -1,2 +1,65 @@
 #!/bin/bash
+
+# migrate old files to new location
+if [ ! -d "$HOME/.config/Cemu" ]; then
+  mkdir "$HOME/.config/Cemu"
+fi
+
+if [ -f "$HOME/.local/share/Cemu/settings.xml" ]; then
+  mv -b "$HOME/.local/share/Cemu/settings.xml" "$HOME/.config/Cemu"
+fi
+
+if [ -d "$HOME/.local/share/Cemu/controllerProfiles" ]; then
+  mv -b "$HOME/.local/share/Cemu/controllerProfiles" "$HOME/.config/Cemu"
+fi
+
+if [ -d "$HOME/.local/share/Cemu/gameProfiles" ]; then
+  mv -b "$HOME/.local/share/Cemu/gameProfiles" "$HOME/.config/Cemu"
+fi
+
+if [ -d "$HOME/.local/share/Cemu/debugger" ]; then
+  mv -b "$HOME/.local/share/Cemu/debugger" "$HOME/.config/Cemu"
+fi
+
+if [ -f "$HOME/.local/share/Cemu/network_services.xml" ]; then
+  mv -b "$HOME/.local/share/Cemu/network_services.xml" "$HOME/.config/Cemu"
+fi
+
+if [ -d "$HOME/.local/share/Cemu/shaderCache" ]; then
+  mv -b "$HOME/.local/share/Cemu/shaderCache" "$HOME/.cache/Cemu"
+fi
+
+# migrate old files to new location
+if [ ! -d "$HOME/.config/Cemu" ]; then
+  mkdir "$HOME/.config/Cemu"
+fi
+
+if [ -f "$HOME/.local/share/Cemu/settings.xml" ]; then
+  mv -b "$HOME/.local/share/Cemu/settings.xml" "$HOME/.config/Cemu"
+fi
+
+if [ -d "$HOME/.local/share/Cemu/controllerProfiles" ]; then
+  mv -b "$HOME/.local/share/Cemu/controllerProfiles" "$HOME/.config/Cemu"
+fi
+
+if [ -d "$HOME/.local/share/Cemu/gameProfiles" ]; then
+  mv -b "$HOME/.local/share/Cemu/gameProfiles" "$HOME/.config/Cemu"
+fi
+
+if [ -d "$HOME/.local/share/Cemu/debugger" ]; then
+  mv -b "$HOME/.local/share/Cemu/debugger" "$HOME/.config/Cemu"
+fi
+
+if [ -f "$HOME/.local/share/Cemu/network_services.xml" ]; then
+  mv -b "$HOME/.local/share/Cemu/network_services.xml" "$HOME/.config/Cemu"
+fi
+
+if [ -f "$HOME/.local/share/Cemu/Cemu" ]; then
+  rm "$HOME/.local/share/Cemu/Cemu"
+fi
+
+if [ -f "$HOME/.local/share/Cemu/Cemu_release" ]; then
+  rm "$HOME/.local/share/Cemu/Cemu_release"
+fi
+
 /opt/Cemu/Cemu_release
